7ef55b43f4b354b595595a8e46eefd3ce60c25b7
kimoyerr <kimoyerr@gmail.com>
2024-06-08 07:49:51 +0000

    Reference to torchtitan

--- plm_train/model.py
+++ plm_train/model.py
@@ -1,11 +1,4 @@
-# Copyright (c) Meta Platforms, Inc. and affiliates.
-# All rights reserved.
-#
-# This source code is licensed under the BSD-style license found in the
-# LICENSE file in the root directory of this source tree.
-#
-# Llama 2 is licensed under the LLAMA 2 Community License,
-# Copyright (c) Meta Platforms, Inc. All Rights Reserved.
+# From: https://github.com/pytorch/torchtitan/blob/main/torchtitan/models/llama/model.py
 
 
 from dataclasses import dataclass
