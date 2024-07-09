06a88d258efe83882190b9c29d99702ae4a7cbf3
kimoyerr <kimoyerr@gmail.com>
2024-07-09 06:40:14 +0000

    Adds weighted cross-entropy and F1-socre metrics

--- plm_train/train_utils.py
+++ plm_train/train_utils.py
@@ -69,4 +69,33 @@ def build_optimizer(model, job_config: JobConfig):
 
 # loss fn can be shared by pipeline-parallel or non-pp execution
 def loss_fn(pred, labels, reduction="mean"):
-    return F.cross_entropy(pred.flatten(0, 1), labels.flatten(0, 1), reduction=reduction)
+        return F.cross_entropy(pred.flatten(0, 1), labels.flatten(0, 1), reduction=reduction)
+
+# loss fn can be shared by pipeline-parallel or non-pp execution
+# Weighted to handle class imbalance
+def loss_fn(pred, labels, class_weights, reduction="mean"):
+        preds = pred.flatten(0, 1)
+        labels = labels.flatten(0, 1)
+        class_weights = class_weights.to(preds.device)
+
+        return F.cross_entropy(preds, labels, weight=class_weights, reduction=reduction)
+
+# F1-score        
+def calc_f1_score(pred, labels, pos_label, neg_label):
+    # Get probabilities
+    log_probs = F.softmax(pred, dim=-1)
+    # Assign multi-class labels
+    pred_labels = log_probs.argmax(dim=-1)
+    # Filter only to labels that are either 4 or 5
+    sel_label_mask = (labels==pos_label) | (labels==neg_label)
+
+    # True positive if labels==pos_labels
+    true_positives = torch.sum((pred_labels[sel_label_mask] == pos_label).float() * (labels[sel_label_mask] == pos_label).float())
+    false_positives = torch.sum((pred_labels[sel_label_mask] == pos_label).float() * (labels[sel_label_mask] == neg_label).float())
+    true_negatives = torch.sum((pred_labels[sel_label_mask] == neg_label).float() * (labels[sel_label_mask] == neg_label).float())
+    false_negatives = torch.sum((pred_labels[sel_label_mask] == neg_label).float() * (labels[sel_label_mask] == pos_label).float())
+    precision = true_positives/(true_positives + false_positives + 1e-6)
+    sensitivity = true_positives/(true_positives + false_negatives + 1e-6)
+    
+    f1_score = 2 * (precision * sensitivity) / (precision + sensitivity + 1e-6)
+    return f1_score, precision, sensitivity, true_positives, false_positives, true_negatives, false_negatives
