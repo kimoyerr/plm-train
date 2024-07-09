be9ab2797c868fbb88222f38ad01e783336d021b
kimoyerr <kimoyerr@gmail.com>
2024-07-09 06:38:12 +0000

    Adds multiple fasta files for input. Each fasta file could be one mode of data like sequence, function etc

--- plm_train/data_utils.py
+++ plm_train/data_utils.py
@@ -1,6 +1,11 @@
+import os
+import warnings
+import random
 import pickle
 from typing import Any, Dict, List, Optional
 from datasets.distributed import split_dataset_by_node
+from functools import partial
+from typing import Callable, List, Optional
 
 import torch
 from torch.distributed.checkpoint.stateful import Stateful
@@ -12,7 +17,16 @@ from datasets.distributed import split_dataset_by_node
 from torchtitan.datasets.tokenizer import Tokenizer
 from torchtitan.logging_utils import logger
 
+from fasta_utils import fasta_to_json
 
+
+# Create json files for training and validation
+def split_fasta_to_json(data_dir, input_fasta_files, output_dir, num_training_seq=1000, num_validation_seq=100):
+    random_seed = 42
+    # Split fasta into json
+    output_counts = fasta_to_json(input_fasta_files, output_dir, ntrain_seq=num_training_seq, nval_seq=num_validation_seq, random_seed=random_seed)
+
+    return output_counts
 
 
 # Modified from torchtitan hf_datasets.py
@@ -40,6 +54,7 @@ class HuggingFaceProteinDataset(IterableDataset, Stateful):
         dataset_path: Optional[str],
         dataset_split: Optional[str],
         tokenizer: Tokenizer,
+        catalytic_tokenizer: Optional[Tokenizer],
         seq_len: int = 2048,
         world_size: int = 1,
         rank: int = 0,
@@ -52,6 +67,7 @@ class HuggingFaceProteinDataset(IterableDataset, Stateful):
         self.dataset_name = dataset_name
         self._data = split_dataset_by_node(ds, rank, world_size)
         self._tokenizer = tokenizer
+        self._catalytic_tokenizer = catalytic_tokenizer
         self.seq_len = seq_len
         self.infinite = infinite
         self.num_repeats = 0
@@ -59,6 +75,7 @@ class HuggingFaceProteinDataset(IterableDataset, Stateful):
         # variables for checkpointing
         self._sample_idx = 0
         self._all_tokens: List[int] = []
+        self._all_catalytic_tokens: List[int] = []
 
     def __iter__(self):
         max_buffer_token_len = 1 + self.seq_len
@@ -68,14 +85,26 @@ class HuggingFaceProteinDataset(IterableDataset, Stateful):
                 sample_text = sample["seq"]
                 sample_tokens = self._tokenizer.encode(sample_text)
                 self._all_tokens.extend(sample_tokens)
+                # Catalytic tokens if sample["catayltic_sites"] is available
+                if sample["catalytic_sites"]:
+                    sample_catalytic_tokens = self._catalytic_tokenizer.encode(sample["catalytic_sites"])
+                    self._all_catalytic_tokens.extend(sample_catalytic_tokens)
                 self._sample_idx += 1
 
-                while len(self._all_tokens) >= max_buffer_token_len:
+                while len(self._all_tokens) >= max_buffer_token_len:  # Keep going while the current tokens are larger than the seq_len for the transformer
                     x = torch.LongTensor(self._all_tokens[:max_buffer_token_len])
                     # update tokens to the remaining tokens
                     self._all_tokens = self._all_tokens[max_buffer_token_len:]
-                    input = x[:-1]
-                    label = x[1:]
+                    # if self._all_catalytic_tokens:
+                    if 1:
+                        y = torch.LongTensor(self._all_catalytic_tokens[:max_buffer_token_len])
+                        self._all_catalytic_tokens = self._all_catalytic_tokens[max_buffer_token_len:]
+                    # if not self._all_catalytic_tokens:
+                    #     print("No catalytic tokens")
+                    input = (x[:-1], y[:-1])
+                    label = (x[1:], y[1:])
+                    # input = (x[:-1], y[:-1]) if self._all_catalytic_tokens else x[:-1]  # Return a tuple if catalytic tokens are available
+                    # label = (x[1:], y[1:]) if self._all_catalytic_tokens else x[1:]  # Return a tuple if catalytic tokens are available
                     yield input, label
 
             if not self.infinite:
@@ -110,9 +139,10 @@ class HuggingFaceProteinDataset(IterableDataset, Stateful):
     def load_state_dict(self, state_dict):
         self._sample_idx = state_dict["sample_idx"]
         self._all_tokens = state_dict["token_buffer"]
+        self._all_catalytic_tokens = state_dict.get("catalytic_token_buffer", [])
 
     def state_dict(self):
-        return {"token_buffer": self._all_tokens, "sample_idx": self._sample_idx}
+        return {"token_buffer": self._all_tokens, "catalytic_token_buffer": self._all_catalytic_tokens, "sample_idx": self._sample_idx}
 
 
 class DPAwareDataLoader(StatefulDataLoader, Stateful):
@@ -147,6 +177,7 @@ def build_hf_data_loader(
     dataset_path: Optional[str],
     dataset_split: Optional[str],
     tokenizer: Tokenizer,
+    catalytic_tokenizer: Optional[Tokenizer],
     batch_size: int,
     seq_len: int,
     world_size,
@@ -154,7 +185,7 @@ def build_hf_data_loader(
     infinite: bool = True,
 ):
     hf_ds = HuggingFaceProteinDataset(
-        dataset_name, dataset_path, dataset_split, tokenizer, seq_len, world_size, rank, infinite
+        dataset_name, dataset_path, dataset_split, tokenizer, catalytic_tokenizer, seq_len, world_size, rank, infinite
     )
 
     return DPAwareDataLoader(rank, hf_ds, batch_size=batch_size)
--- plm_train/fasta_utils.py
+++ plm_train/fasta_utils.py
@@ -1,6 +1,8 @@
+import os
 import random
 from collections import namedtuple
 import json
+from Bio import SeqIO
 
 
 def sample_seqs(input_fasta, n_seq=100):
@@ -41,31 +43,70 @@ def sample_seqs(input_fasta, n_seq=100):
     return sampled_seqs
 
 
-def fasta_to_json(input_fasta, train_json, val_json, ntrain_seq=100, nval_seq=100, random_seed=42):
+def fasta_to_json(input_fasta_files, output_dir, ntrain_seq=100, nval_seq=100, random_seed=42):
     """Write n_seq sequences from input_fasta to output_json
 
     Args:
-        input_fasta (str): fasta file path with protein sequences
+        input_fasta (str): fasta file paths with protein sequences
         output_csv (str): csv file path to write sequences to
         n_seq (int, optional): Number of sequences to sample. Defaults to 100.
 
     Returns:
-        str: output_cjson file path
+        str: output_json file path
     """
 
-    protein_data = sample_seqs(input_fasta, n_seq=ntrain_seq + nval_seq)
-    # Create a dictionary with "seq" and "id" keys
-    protein_data = [protein._asdict() for protein in protein_data]
-    # Random shuffle
-    random.seed(random_seed)
-    random.shuffle(protein_data)
-    # Split into training and validation data
-    protein_data_train = protein_data[:ntrain_seq]
-    protein_data_val = protein_data[ntrain_seq:(ntrain_seq + nval_seq)]
-    # Write to json with "seq" and "id" keys
-    with open(train_json, "w") as f:
-        json.dump(protein_data_train, f)
-    with open(val_json, "w") as f:
-        json.dump(protein_data_val, f)
+
+    # Convert fasta files to hierarchial dict to keep track of the sequences that correspond to the same id
+    fasta_records = {}  # Nested dictionary
+    for f_key, f_value in input_fasta_files.items():
+        all_records = list(SeqIO.parse(f_value, "fasta"))
+        # Dictionary
+        for record in all_records:
+            # Check if key exists
+            if record.id in fasta_records:
+                # Add new key
+                fasta_records[record.id][f_key] = str(record.seq)
+            else:
+                fasta_records[record.id] = {f_key: str(record.seq)}
+
+    # Check for each key, the length of sequence is the same as the length of catalytic_sites
+    for record in fasta_records.values():
+        assert len(record["sequence"]) == len(record["catalytic_sites"])
     
-    return train_json, val_json
+    # Randomly sample from fasta records
+    random.seed(random_seed)
+    shuffled_keys = list(fasta_records.keys())
+    random.shuffle(shuffled_keys)
+    
+    # if ntrain_seq is "all" and nval_seq is 0, return all sequences
+    if ntrain_seq == "all":
+        ntrain_seq = len(shuffled_keys)
+        nval_seq = 0
+    elif nval_seq == "all":
+        ntrain_seq = 0
+        nval_seq = len(shuffled_keys)
+
+    protein_data_train = [{"id":key, "seq":fasta_records[key]["sequence"], "catalytic_sites":fasta_records[key]["catalytic_sites"]} for key in shuffled_keys[:ntrain_seq]]
+    protein_catalytic_data_train = [{"id":key, "seq":fasta_records[key]["catalytic_sites"]} for key in shuffled_keys[:ntrain_seq]]
+
+    protein_data_val = [{"id":key, "seq":fasta_records[key]["sequence"], "catalytic_sites":fasta_records[key]["catalytic_sites"]} for key in shuffled_keys[ntrain_seq:ntrain_seq+nval_seq]]
+    protein_catalytic_data_val = [{"id":key, "seq":fasta_records[key]["catalytic_sites"]} for key in shuffled_keys[ntrain_seq:ntrain_seq+nval_seq]]
+
+    # Write to json with "seq" and "id" keys
+    # Create new folders in the output dir
+    seq_folder = output_dir + "/seq"
+    catalytic_folder = output_dir + "/catalytic"
+    if ntrain_seq != 0:
+        with open(f"{output_dir}/train.json", "w") as f:
+            json.dump(protein_data_train, f)
+    if nval_seq != 0:
+        with open(f"{output_dir}/val.json", "w") as f:
+            json.dump(protein_data_val, f)
+    # with open(f"{catalytic_folder}/train.json", "w") as f:
+    #     json.dump(protein_catalytic_data_train, f)
+    # with open(f"{catalytic_folder}/val.json", "w") as f:
+    #     json.dump(protein_catalytic_data_val, f)
+
+    return {"num_train_seq": ntrain_seq, "num_val_seq": nval_seq}
+
+    
